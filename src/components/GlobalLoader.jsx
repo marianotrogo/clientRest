b3b4f502cde823119d28afe32151dc13568f3f04
marianotrogo <marianoemmanuel_22@hotmail.com>
2025-09-28 11:50:53 -0300

    changed color white background loader

--- src/components/GlobalLoader.jsx
+++ src/components/GlobalLoader.jsx
@@ -8,7 +8,7 @@ export default function GlobalLoader() {
   if (!isLoading) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-white flex items-center justify-center z-50">
       <Loader text={text} />
     </div>
   );
